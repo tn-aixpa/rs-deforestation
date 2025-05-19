23d971d66461e58f7e4e8d5217dde5911c3b5d8b
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-19 12:55:05 +0200

    Fixes for updated rsde-tools

--- src/launch.sh
+++ src/launch.sh
@@ -3,12 +3,10 @@ ls -la /shared
 cd ~
 pwd
 source .bashrc
-echo "Init conda.."
-source activate rsde
 echo "GDAL version:"
-python --version
 gdal-config --version
-#printenv
+python --version
+printenv
 cd /app
 python main.py "{'input1':'bosco', 'input2': 'data', 'input3':['2018', '2019'], 'input4': 'deforestation_output'}"
 exit
