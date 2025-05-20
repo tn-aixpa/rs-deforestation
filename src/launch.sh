c2d9994bc401729c8dfdf7a94fa192d9651bf199
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-20 14:38:19 +0200

    Update launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -6,7 +6,9 @@ source .bashrc
 echo "GDAL version:"
 gdal-config --version
 python --version
-printenv
+#printenv
 cd /app
-python main.py "{'input1':'bosco', 'input2': 'data', 'input3':['2018', '2019'], 'input4': 'deforestation_output'}"
+echo "{'input1': '$1', 'input2': '$2', 'input3':'$3', 'input4': '$4'}"
+#python main.py "{'input1':'bosco', 'input2': 'data', 'input3':['2018', '2019'], 'input4': 'deforestation_output'}"
+python main.py "{'input1': '$1', 'input2': '$2', 'input3':$3, 'input4': '$4'}"
 exit
