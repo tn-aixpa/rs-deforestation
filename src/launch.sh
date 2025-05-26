14a508cf664d06e014c4f18c9b8cdaed2f29201b
Nawaz Khurshid <nawaz1981@gmail.com>
2025-05-26 14:31:00 +0200

    Fixes for miniforge

--- src/launch.sh
+++ src/launch.sh
@@ -12,8 +12,8 @@ echo "PROJ_LIB"
 echo $PROJ_LIB
 cd /app
 echo "{'input1': '$1', 'input2': '$2', 'input3':$3, 'input4': '$4'}"
-export PROJ_LIB=/home/nonroot/miniconda3/share/proj
-export GDAL_DATA=/home/nonroot/miniconda3/share/gdal
+export PROJ_LIB=/home/nonroot/miniforge3/share/proj
+export GDAL_DATA=/home/nonroot/miniforge3/share/gdal
 echo "GDAL DATA AFTER EXPORT:"
 echo $GDAL_DATA
 echo "PROJ_LIB AFTER EXPORT"
